6cf377df17874ffb6c559d06f6caf6bb3708b0e2
Luwina P <pluwina141197@gmail.com>
2021-10-05 10:29:10 +0530

    Hibernate Assignemnt Student details

---
+++ assignment/Student.java
@@ -0,0 +1,20 @@
+package assignment;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Student implements Serializable{
+@Id
+private int rollno;
+private String name;
+private long phoneNo;
+private int standard;
+
+
+}
